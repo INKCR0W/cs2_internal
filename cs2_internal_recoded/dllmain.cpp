19d148745e60098d6caef87baa13530235720e90
InkCrow <inkcrow1337@gmail.com>
2024-08-14 14:20:30 +0800

    recoding

---
+++ cs2_internal_recoded/dllmain.cpp
@@ -0,0 +1,30 @@
+﻿#include <Windows.h>
+#include <iostream>
+
+#include "utils/random.hpp"
+
+bool __stdcall DllMain(HMODULE hModule, DWORD  ul_reason_for_call, LPVOID lpReserved)
+{
+#ifdef _DEBUG
+	AllocConsole();
+
+	FILE* fDummy;
+	freopen_s(&fDummy, "CONOUT$", "w", stdout);
+	freopen_s(&fDummy, "CONOUT$", "w", stderr);
+	freopen_s(&fDummy, "CONIN$", "r", stdin);
+
+	SetConsoleTitle(utils::randomString(0ULL).c_str());
+
+	cheat::cs2_internal::get_instance().set_console(GetConsoleWindow());
+#endif
+
+
+	DisableThreadLibraryCalls(hModule);
+
+    if (ul_reason_for_call == DLL_PROCESS_ATTACH) {
+
+    }
+
+    return TRUE;
+}
+
---
+++ cs2_internal_recoded/utils/random.hpp
@@ -0,0 +1,39 @@
+#pragma once
+
+// used: string
+#include <string>
+// used: random_device mt19937 uniform_int_distribution
+#include <random>
+// used: generate
+#include <algorithm>
+
+namespace utils {
+    /// <summary>
+    /// Return a random string¡£
+    /// </summary>
+    /// <param name="length">
+    /// The <c>length</c> of string
+    /// </param>
+    /// <param name="charset">
+    /// Char set for string
+    /// </param>
+    /// <returns>The <c>random</c> string</returns>
+    const std::string randomString(size_t length = 0, const std::string& charset = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789~!@#$%^&*()_+=-[]{};':,.<>/?") noexcept
+    {
+        std::random_device rd;
+        std::mt19937 generator(rd());
+
+        if (length == 0) {
+            std::uniform_int_distribution<std::size_t> length_dist(16, 32);
+            length = length_dist(generator);
+        }
+
+        std::string result;
+        result.resize(length);
+
+        std::uniform_int_distribution<std::size_t> distribution(0, charset.size() - 1);
+        std::generate(result.begin(), result.end(), [&]() { return charset[distribution(generator)]; });
+
+        return result;
+    }
+}
