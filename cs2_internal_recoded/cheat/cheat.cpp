815423f2e69f712d123e722c60089084946af337
InkCrow <inkcrow1337@gmail.com>
2024-08-25 15:24:59 +0800

    TODO

--- cs2_internal_recoded/cheat/cheat.cpp
+++ cs2_internal_recoded/cheat/cheat.cpp
@@ -1,3 +1,15 @@
 #include "cheat.hpp"
 
+#include <iostream>
 
+namespace cheat {
+	bool cs2_internal::setup() {
+		// TODO
+		return true;
+	}
+
+	bool cs2_internal::run() {
+		// TODO
+		return true;
+	}
+}
--- cs2_internal_recoded/cheat/cheat.hpp
+++ cs2_internal_recoded/cheat/cheat.hpp
@@ -9,5 +9,10 @@ namespace cheat {
 		cs2_internal(const cs2_internal& copy) = delete;
 		cs2_internal& operator=(const cs2_internal& other) = delete;
 
+		// The setup function, run this before everything
+		bool setup();
+		// This one should run in a loop
+		bool run();
+
 	};
 }
